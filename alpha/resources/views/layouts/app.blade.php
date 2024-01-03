27fc92a6e31fb786b25e476c3c3962003e270057
jlal.osama.rezeqat@gmail.com <Aboosama@212>
2024-01-03 12:03:12 +0200

    home

--- alpha/resources/views/layouts/app.blade.php
+++ alpha/resources/views/layouts/app.blade.php
@@ -19,70 +19,67 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img  class="nav-link active" aria-current="page" class="img" src="img/logo.png" />
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <a class="navbar-brand" href="{{ url('/') }}"><img src="img/logo.png" alt=""></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse collapse navbar-collapse justify-content-center d-flex " id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                   
-                    <li class="nav-item">
-                        <a  class="nav-link active reg "   aria-current="page" href="{{ url('/') }} ">الرئيسية</a>       
-                    </li>
-                    <li class="nav-item">
-                    <a  class="nav-link active" aria-current="page" href="">حول الفا</a>       
-                     </li>
-                    <li class="nav-item">
-                    <a  class="nav-link active" aria-current="page"href="">الدورات</a>
-                    </li>
-                    <li class="nav-item">
-                    <a  class="nav-link active" aria-current="page" href="">اتصل بنا</a>
-                    </li>
-                    <li>
-                    @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a  class=" nav-link active reg"  aria-current="page" href="{{ route('register') }} ">تسجيل</a>
-    
-                            <a   class="nav-link active" aria-current="page" href="{{ route('login') }}" >تسجيل الدخول </a>
-                        </li>
-                    @endif
-    
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <li> </li>
-                        </li>
-                    @endif
-                    </li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-    
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-    
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active reg" aria-current="page" href="{{ url('/') }}">الرئيسية</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">الدورات</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">حول الفا</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">اتصل بنا</a>
+                  </li>
+                  @guest
+                  @if (Route::has('login'))
+                      <li class="nav-item">
+                          <a  class=" nav-link active reg"  aria-current="page" href="{{ route('register') }} ">تسجيل</a>
+      
+                          <a   class="nav-link active" aria-current="page" href="{{ route('login') }}" >تسجيل الدخول </a>
+                      </li>
+                  @endif
+      
+                  @if (Route::has('register'))
+                      <li class="nav-item">
+                          <li> </li>
+                      </li>
+                  @endif
+                  </li>
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }}
+                      </a>
+      
+                      <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+      
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+              @endguest
+                 
                 </ul>
                 
               </div>
             </div>
           </nav>
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
--- alpha/resources/views/welcome.blade.php
+++ alpha/resources/views/welcome.blade.php
@@ -3,34 +3,93 @@
 
     @vite(['resources/css/welcome.css'])
     @section('content')
-    <div id="carouselExampleDark" class="slider carousel carousel-dark slide" data-bs-ride="carousel">
+    {{-- slider home --}}
+    <div id="carouselExampleSlidesOnly" class="carousel slider slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+        </div>
+      </div>
+    </div>
+    {{-- end slider home --}}
+    {{-- <span  class="w-75 p-3 border slider d-flex card-bord  justify-content-around rounded p-3 mb-2  text-white "> --}}
+<div class="contener ">
+    <div class="mt">
+      <h3 class="text-center ">الدورات الاكثر طلبا </h3>
+      <h5 class="text-center ">اختر دورات التوجيهي التي تناسبك وتساعدك على زيادة معدلك</h5>
+    </div>
+    {{-- card course --}}
+    <div class=" card-box ovarflow card-w  slider">
+    
+      <div class="row row-cols-1 card-w    row-cols-md-3 ">
+        <div class="col">
+          <div class="card">
+            <img src="img/img_avatar.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="img/img_avatar.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="img/img_avatar.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="img/img_avatar.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+    
+    {{-- end card course --}}
+    <div class="mt-1">
+      <h3 class="text-center ">معلمي منصة الفا </h3>
+      <h5 class="text-center ">نفتخر في ألفا بتواجد  أفضل المُدرسين على مستوى الوطن!</h5>
+    </div>
+
+    {{-- slide tetcher --}}
+    <div id="carouselExampleDark" class="carousel carousel-dark slider slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-      <div class=" carousel-inner">
+      <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
-          <img src="..." class="d-block w-100" alt="...">
+          <img src="img/slide2.png" class="d-block w-100 h-100" alt="...">
           <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+           
           </div>
         </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="..." class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
+      
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -41,42 +100,127 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
+    {{-- end slide tetcher --}}
+    {{-- qustion  --}}
+    <div class="mt slider card-text">
+      <p>
+       
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          كيف يُمكنني الإلتحاق بالدورات على منصّة ألفا؟ 
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+          من خلال النقر على زر “اطلب بطاقتك” المتوفّر في كافة الصفحات الخاصّة بالدورات وتعبئة المعلومات المطلوبة أو من خلال
+          التواصل مع فريق المبيعات عبر الواتس آب.
+        </div>
+      </div>
 
-    <div class="mt">
-      <h3 class="text-center mt">الدورات الاكثر طلبا </h3>
-      <h5 class="text-center ">اختر دورات التوجيهي التي تناسبك وتساعدك على زيادة معدلك</h5>
+      <p>
+       
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+          كيف يُمكنني الإلتحاق بالدورات على منصّة ألفا؟ 
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample1">
+        <div class="card card-body">
+          من خلال النقر على زر “اطلب بطاقتك” المتوفّر في كافة الصفحات الخاصّة بالدورات وتعبئة المعلومات المطلوبة أو من خلال
+          التواصل مع فريق المبيعات عبر الواتس آب.
+        </div>
+      </div>
     </div>
-    <span class=" slider  border d-flex card-bord  justify-content-around rounded p-3 mb-2  text-white ">
-      <div class="card-group">
-        <div class="card">
-          <img src="img/img_avatar.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title ">دورة الرياضيات للفرع العلمي </h5>
-            <p class="card-text">تشمل الدورة شرحا كاملا لمادة الرياضيات الفرع العلمي </p>
-            <button>تعرف على المزيد </button>
+    {{-- end qustion --}}
+
+    {{-- foter --}}
+   <!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="footer">
+
+  <footer class=" text-center container  text-white">
+    <!-- Grid container -->
+    <div class=" p-4">
+      <!--Grid row-->
+      <div class="row ">
+        <!--Grid column-->
+        <div class="col-lg-3 ">
+
+          <div class="rounded-circle  d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
+            <img src="img/logofooter.jpeg" height="70" alt=""
+                 loading="lazy" />
           </div>
         </div>
-        <div class="card">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <button>تعرف على المزيد </button>
-          </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 ">
+
+          <ul class="list-unstyled">
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>منصّة ألفا - alpha.ps</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i> alpha.ps</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>منصّة ألفا - alpha.ps</a>
+            </li>
+        
+          </ul>
         </div>
-        <div class="card">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <button>تعرف على المزيد </button>
-          </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 ">
+
+          <ul class="list-unstyled">
+            <li class="mb-2">
+              <a href="{{ url('/') }}" class="text-white"><i class="fas fa-paw pe-3"></i>الرئيسيّة</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>الدورات</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>حول ألفا</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>اتصل بنا</a>
+            </li>
+          </ul>
         </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 ">
+
+          <ul class="list-unstyled">
+            <li>
+              <p><i class="fas fa-map-marker-alt pe-2"></i>تواصل معنا وابدأ رحلتـك</p>
+              <p><i class="fas fa-map-marker-alt pe-2"></i>للحصول على مُعدّل 99.7</p>
+            </li>
+            <li>
+              <p><i class="fas fa-phone pe-2"></i>info@alpha.ps</p>
+            </li>
+            <li>
+              <p><i class="fas fa-envelope pe-2 mb-0"></i>(+970) 597-618-504</p>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
       </div>
-      <div>
-     
-      </div>
-    </span>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center " >
+      Copyright © 2024 alpha All Rights Reserved. 
+       </div>
+    <!-- Copyright -->
+  </footer>
+
+</div>
+<!-- End of .container -->
+    <!-- Footer -->
+
 
     @endsection
 
