16b9cf62713e7b6279d19aea27145dbd705275f1
unknown <jlal.osama.rezeqat@gmail.com>
2024-01-03 15:19:05 +0200

    vvvv

--- alpha/resources/views/welcome.blade.php
+++ alpha/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     @vite(['resources/css/welcome.css'])
     @section('content')
     {{-- slider home --}}
-    <div id="carouselExampleSlidesOnly" class="carousel slider slide" data-ride="carousel">
+    <div id="carouselExampleSlidesOnly" class="carousel slider  slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+          <img class="d-block w-100" src="img/slide1.png" alt="First slide">
         </div>
         <div class="carousel-item">
           <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
@@ -76,35 +76,40 @@
     </div>
 
     {{-- slide tetcher --}}
-    <div id="carouselExampleDark" class="carousel carousel-dark slider slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-          <img src="img/slide2.png" class="d-block w-100 h-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-           
-          </div>
+    <div id="carouselExampleIndicators" class="carousel slider  slide" data-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block  w-100" src="img/slide2.png" alt="First slide">
         </div>
-      
+        <div class="carousel-item">
+          <img class="d-block w-100" src="img/slide2.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block  w-100" src="img/slide2.png" alt="Third slide">
+        </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     {{-- end slide tetcher --}}
     {{-- qustion  --}}
-    <div class="mt slider card-text">
+
+    <div class="mt-3  card-text">
+     <h2 class="card-text">الاسئلة الشائعة</h2>
+
       <p>
-       
+
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           كيف يُمكنني الإلتحاق بالدورات على منصّة ألفا؟ 
         </button>
@@ -133,7 +138,7 @@
 
     {{-- foter --}}
    <!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="footer">
+<div class="footer carousel slide ">
 
   <footer class=" text-center container  text-white">
     <!-- Grid container -->
