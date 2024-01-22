195056d0e51b45969707f81c15cc3f95c0e04c54
jlal.osama.rezeqat@gmail.com <Aboosama@212>
2024-01-22 17:26:30 +0200

    user dashbord
    ....

--- alpha/resources/views/dashboard.blade.php
+++ alpha/resources/views/dashboard.blade.php
@@ -1,21 +1,74 @@
 @extends('layouts.app')
 
 @section('content') 
-    <div class="user-dashbord">
+    <div class="user-dashbord" id="user-dashbord">
     <div class="box-dashbord" id="box-dashbord">
+        <div class="row"  id="">
+            <!--Grid column-->
+            <div class="col-lg-2 ">
+    
+              <div class="rounded-circle  d-flex align-items-center justify-content-center mb-4 mx-auto">
+                <img src="img/dashbord.png"  alt=""
+                     loading="lazy" />
+              </div>
+            </div>
+            <!--Grid column-->
+    
+            <!--Grid column-->
+            <div class="col-lg-5 ">
+    
+              <ul class="list-unstyled user_name">
+                <li class="mb-2">
+                  <h3>مرحباً بعودتك، </h2> 
+                  <h3> {{ Auth::user()->farestname .' '. Auth::user()->lastname}}</h2>
+                </li>
+                <li class="mb-2">
+                  <p>  <img src="img/branch.png" alt=""> الفرع :  {{ Auth::user()->branch }}  </p>
+                  <p> <img src="img/phone-dashbord.png" alt=""> رقم الهاتف : {{ Auth::user()->phone }} </p>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+    
+            <!--Grid column-->
+            <div class="col-lg-5 ">
+    
+              <ul class="list-unstyled  information-dashbord">
+                  <li>
+                      <a class="link-edit" href="">تعديل الملف الشّخصي</a>
+                  </li>
+                <li class="mb-2">
+                  <p> <img src="img/address-dashbord.png" alt=""> المحافظة :  {{ Auth::user()->Governorate }}</p>
+                  <p> <img src="img/email-dashbord.png"> الايميل  : {{ Auth::user()->email }}  </p>   
+                </li>
+              
+              </ul>
+            </div>
+            <!--Grid column-->
+    
+            <!--Grid column-->
+           
+          </div>
+    </div>
+    </div>
 
-            <div>
+  
+        
+
+@endsection
+
+{{-- <div>
                 <img src="img/dashbord.png" class="img-fluid   img-dashbord" alt="">
             </div>
 
             <div class=" dir">
                 <div class="user_name">
                     <h2>مرحباً بعودتك، </h2> 
-                    <h2> {{ Auth::user()->name }}</h2>
+                    <h2> {{ Auth::user()->farestname .' '. Auth::user()->lastname}}</h2>
                 </div>
                 <div class="information-dashbord dir">
-                    <p>الفرع :  {{ Auth::user()->branch }}  </p>
-                    <p>رقم الهاتف : {{ Auth::user()->phone }} </p>
+                   <p>  <img src="img/branch.png" alt=""> الفرع :  {{ Auth::user()->branch }}  </p>
+                    <p> <img src="img/phone-dashbord.png" alt=""> رقم الهاتف : {{ Auth::user()->phone }} </p>
                 </div>
                 
             </div>
@@ -24,9 +77,9 @@
                     <br>
                 <a href="">تعديل الملف الشّخصي</a>
                 </div>
-                <div class="information-dashbord dir">
-                <p> المحافظة :  {{ Auth::user()->Governorate }}</p>
-                <p>الايميل : {{ Auth::user()->email }}  </p>         
+                <div class="information-dashbord dir" id="information-dashbord1">
+                <p> <img src="img/address-dashbord.png" alt=""> المحافظة :  {{ Auth::user()->Governorate }}</p>
+                <p> <img src="img/email-dashbord.png"> الايميل  : {{ Auth::user()->email }}  </p>         
             </div>
                 
             </div>
@@ -39,7 +92,63 @@
 
             <div class="dir">
             <p>علامات الإمتحانات:</p>
-            </div>
-    </div>
+            </div> --}}
 
-@endsection
+
+
+
+
+            {{--  --}}
+
+
+            {{-- 
+        {{-- <div class=" p-4"> --}}
+            <!--Grid row-->
+            {{-- <div class="row"  id="">
+                <!--Grid column-->
+                <div class="col-lg-2 ">
+        
+                  <div class="rounded-circle  d-flex align-items-center justify-content-center mb-4 mx-auto">
+                    <img src="img/dashbord.png"  alt=""
+                         loading="lazy" />
+                  </div>
+                </div>
+                <!--Grid column-->
+        
+                <!--Grid column-->
+                <div class="col-lg-5 ">
+        
+                  <ul class="list-unstyled user_name">
+                    <li class="mb-2">
+                      <h3>مرحباً بعودتك، </h2> 
+                      <h3> {{ Auth::user()->farestname .' '. Auth::user()->lastname}}</h2>
+                    </li>
+                    <li class="mb-2">
+                      <p>  <img src="img/branch.png" alt=""> الفرع :  {{ Auth::user()->branch }}  </p>
+                      <p> <img src="img/phone-dashbord.png" alt=""> رقم الهاتف : {{ Auth::user()->phone }} </p>
+                    </li>
+                  </ul>
+                </div>
+                <!--Grid column-->
+        
+                <!--Grid column-->
+                <div class="col-lg-5 ">
+        
+                  <ul class="list-unstyled information-dashbord">
+                      <li>
+                          <a href="">تعديل الملف الشّخصي</a>
+  
+                      </li>
+                    <li class="mb-2">
+                      <p> <img src="img/address-dashbord.png" alt=""> المحافظة :  {{ Auth::user()->Governorate }}</p>
+                      <p> <img src="img/email-dashbord.png"> الايميل  : {{ Auth::user()->email }}  </p>   
+                    </li>
+                  
+                  </ul>
+                </div>
+                <!--Grid column-->
+        
+                <!--Grid column-->
+               
+              </div>
+          </div> --}} 
