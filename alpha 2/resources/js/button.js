83732558cc26e5aebc7269888de82b2dfc77ca4c
jlal.osama.rezeqat@gmail.com <Aboosama@212>
2024-07-06 12:47:04 +0300

    ..

--- alpha 2/resources/js/button.js
+++
@@ -1,8 +0,0 @@
-const btnElList = document.querySelectorAll('.btn') ;
-btnElList. forEach(btnEl => {
-    btnEl.addEventListener(' click', () => {
-    document.querySelector('.special')?.classList.remove('special')
-    btnEl.classList.add('special');
-
-})
-});
